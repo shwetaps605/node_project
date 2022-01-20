8cd47469943c2c66d62b216cd551417bd8089714
shwetaps605 <shwetaroy305@gmail.com>
2022-01-21 02:44:12 +0530

    Added SVG and animations and search functionality

--- frontend/src/components/product-list/product-list.component.jsx
+++ frontend/src/components/product-list/product-list.component.jsx
@@ -3,18 +3,36 @@ import './product-list.component.scss'
 import Product from '../product/product.component'
 import ProductsDataService from '../../services/products.service'
 
-const ProductList = ({ products }) => {
+const ProductList = (props) => {
 
-    const [retrievedProducts, setRetreivedProducts] = useState(products)
+    const [products, setProducts] = useState([])
+    const [filterText, setFilterText] = useState("")
 
     useEffect(() => {
-        setRetreivedProducts(products)
-    }, [products])
+        fetchProducts()
+    }, [])
 
-    const refreshProducts = () => {
+    const filterProducts = () => {
+        
+        const filteredProducts = products.filter((product)=>
+            product.name.toLowerCase().includes(filterText.toLowerCase())
+        )
+        setProducts(filteredProducts)
+    }
+
+    const handleSearchInputChange = (e) => {
+        setFilterText(e.target.value)
+        console.log(filterText);
+
+        filterProducts()
+    }
+
+    const fetchProducts = () => {
         ProductsDataService.getAllProducts()
             .then(response => {
-                setRetreivedProducts(response.data.products)
+                //console.log(response.json());
+                //console.log(response.data.products)
+                setProducts(response.data.products)
             })
             .catch(error => console.log(error))
     }
@@ -24,7 +42,7 @@ const ProductList = ({ products }) => {
         ProductsDataService.deleteProduct(productId)
             .then(response => {
                 console.log("Product deleted")
-                refreshProducts()
+                fetchProducts()
             })
             .catch(error => console.log(error))
     }
@@ -32,35 +50,49 @@ const ProductList = ({ products }) => {
 
     return (
 
+        <>
+            <div className="search__product__container">
+                <div>
+                    <input type="text"
+                        name="searchField"
+                        onChange={handleSearchInputChange}
+                        id="searchInput"
+                        placeholder='search product' />
+                    
+                </div>
+            </div>
+            <div className="products__list__container">
+                {
+                    products.map(product => (
+                        <div key={product.id} className="product">
+                            <div
+                                className='product__image'
+                                style={{
+                                    backgroundImage: `url(${product.image})`
+                                }}
+                            />
 
-        <div className="products__list__container">
-            {
-                retrievedProducts.map(product => (
-                    <div key={product.id} className="product">
-                        <div
-                            className='product__image'
-                            style={{
-                                backgroundImage: `url(${product.image})`
-                            }}
-                        />
+                            <div>
+                                <p className='product__name'>{product.name}</p>
+                                <p className='product__price'>{product.price}</p>
+                            </div>
+                            <p className='product__desc'>{product.desc}</p>
+                            <div className='cta'>
+                                <button
+                                    type='button'
+                                    onClick={() => removeProduct(product.id)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+                                        <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" />
+                                    </svg>
+                                </button>
+                            </div>
 
-                        <div>
-                            <p className='product__name'>{product.name}</p>
-                            <p className='product__price'>{product.price}</p>
                         </div>
-                        <p className='product__desc'>{product.desc}</p>
-                        <div className='cta'>
-                            <button
-                                type='button'
-                                onClick={() => removeProduct(product.id)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" /></svg>
-                            </button>
-                        </div>
+                    ))
+                }
+            </div>
 
-                    </div>
-                ))
-            }
-        </div>
+        </>
 
 
     )
