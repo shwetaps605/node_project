03309e604f9f058ce534b7338e5ba0a869347545
shwetaps605 <shwetaroy305@gmail.com>
2022-01-23 23:02:55 +0530

    Added ProductCard component and fixed the product removal method

---
+++ frontend/src/components/add-and-update-product/addOrUpdateProduct.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './addOrUpdateProduct.styles.scss'
+
+const AddorUpdateProduct = (props) => {
+    return
+}
--- frontend/src/components/product-list/product-list.component.jsx
+++ frontend/src/components/product-list/product-list.component.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import './product-list.component.scss'
 import Product from '../product/product.component'
 import ProductsDataService from '../../services/products.service'
+import ProductCard from '../product-card/product-card.component'
 
 const ProductList = (props) => {
 
@@ -160,30 +161,7 @@ const ProductList = (props) => {
             <div className="products__list__container">
                 {
                     products.map(product => (
-                        <div key={product.id} className="product">
-                            <div
-                                className='product__image'
-                                style={{
-                                    backgroundImage: `url(${product.image})`
-                                }}
-                            />
-
-                            <div>
-                                <p className='product__name'>{product.name}</p>
-                                <p className='product__price'>{product.price}</p>
-                            </div>
-                            <p className='product__desc'>{product.desc}</p>
-                            <div className='cta'>
-                                <button
-                                    type='button'
-                                    onClick={() => removeProduct(product.id)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
-                                        <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" />
-                                    </svg>
-                                </button>
-                            </div>
-
-                        </div>
+                        <ProductCard key={product.id} product={product} handleChange={()=>{removeProduct(product.id)}}></ProductCard>
                     ))
                 }
             </div>
